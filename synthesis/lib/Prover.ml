d7c70c602db1f339abb43fc5a71419d7ab5d3eb9
Priya Srikumar <ps735@cornell.edu>
2020-04-07 13:27:20 -0400

    minimal attempt at quantifiying without the library

--- synthesis/lib/Prover.ml
+++ synthesis/lib/Prover.ml
@@ -3,26 +3,35 @@ open Ast
 open Util
 open Packet
 
-let rec expr_to_term expr : Smtlib.term =
+let quant_const expr etyp styp =
+  match styp with
+  | `Valid -> (match etyp with
+      | "var" -> Smtlib.const expr
+      | _ -> raise (Failure "not allowed here"))
+  | `Sat -> (match etyp with
+      | "hole" -> Smtlib.const expr
+      | _ -> raise (Failure "not allowed here"))
+
+let rec expr_to_term expr styp : Smtlib.term =
   match expr with
   | Value (Int (num, _)) -> Smtlib.int_to_term (Bigint.to_int_exn num)
-  | Var (v, _) -> String v
-  | Hole (h, _) -> String h
-  | Plus (e1, e2) -> Smtlib.add (expr_to_term e1) (expr_to_term e2)
-  | Times (e1, e2) -> Smtlib.mul (expr_to_term e1) (expr_to_term e2)
-  | Minus (e1, e2) -> Smtlib.sub (expr_to_term e1) (expr_to_term e2)
+  | Var (v, _) -> quant_const v "var" styp
+  | Hole (h, _) -> quant_const h "hole" styp
+  | Plus (e1, e2) -> Smtlib.add (expr_to_term e1 styp) (expr_to_term e2 styp)
+  | Times (e1, e2) -> Smtlib.mul (expr_to_term e1 styp) (expr_to_term e2 styp)
+  | Minus (e1, e2) -> Smtlib.sub (expr_to_term e1 styp) (expr_to_term e2 styp)
 
-let rec test_to_term test : Smtlib.term =
+let rec test_to_term test styp : Smtlib.term =
   match test with
   | True -> Smtlib.bool_to_term true
   | False -> Smtlib.bool_to_term false
-  | Eq (e1, e2) -> Smtlib.equals (expr_to_term e1) (expr_to_term e2)
-  | Le (e1, e2) -> Smtlib.lte (expr_to_term e1) (expr_to_term e2)
-  | And (t1, t2) -> Smtlib.and_ (test_to_term t1) (test_to_term t2)
-  | Or (t1, t2) -> Smtlib.or_ (test_to_term t1) (test_to_term t2)
-  | Impl (t1, t2) -> Smtlib.implies (test_to_term t1) (test_to_term t2)
-  | Iff (t1, t2) -> Smtlib.equals (test_to_term t2) (test_to_term t1)
-  | Neg t -> Smtlib.not_ (test_to_term t)
+  | Eq (e1, e2) -> Smtlib.equals (expr_to_term e1 styp) (expr_to_term e2 styp)
+  | Le (e1, e2) -> Smtlib.lte (expr_to_term e1 styp) (expr_to_term e2 styp)
+  | And (t1, t2) -> Smtlib.and_ (test_to_term t1 styp) (test_to_term t2 styp)
+  | Or (t1, t2) -> Smtlib.or_ (test_to_term t1 styp) (test_to_term t2 styp)
+  | Impl (t1, t2) -> Smtlib.implies (test_to_term t1 styp) (test_to_term t2 styp)
+  | Iff (t1, t2) -> Smtlib.equals (test_to_term t2 styp) (test_to_term t1 styp)
+  | Neg t -> Smtlib.not_ (test_to_term t styp)
 
 let rec model_to_packet (lst : (Smtlib.identifier * Smtlib.term) list) : Packet.t =
   let name_vals : (string * value) list = (List.map
@@ -33,7 +42,7 @@ let rec model_to_packet (lst : (Smtlib.identifier * Smtlib.term) list) : Packet.
                                                   | _ -> raise (Failure "not a supported model")))                                             lst)
                                              in StringMap.of_alist_exn name_vals
 
-let toZ3String test = test_to_term test |> Smtlib.term_to_sexp |> Smtlib.sexp_to_string
+let toZ3String styp test = test_to_term styp test |> Smtlib.term_to_sexp |> Smtlib.sexp_to_string
 
 let prover = Smtlib.make_solver "/usr/bin/z3"
 
@@ -42,8 +51,8 @@ let check (params : Parameters.t) typ (test : Ast.test) =
   let open Smtlib in
   let st = Time.now() in
   let response = match typ with
-    |`Sat -> assert_ prover (test_to_term test); check_sat_using (UFBV : tactic) prover
-    |`Valid -> assert_ prover (test_to_term (Neg test)); check_sat_using (QFBV : tactic) prover in
+    |`Sat -> assert_ prover (test_to_term test `Sat); check_sat_using (UFBV : tactic) prover
+    |`Valid -> assert_ prover (not_ (test_to_term test `Valid)); check_sat_using (QFBV : tactic) prover in
   let dur = Time.(diff (now()) st) in
   let model =
     if response = Sat then
