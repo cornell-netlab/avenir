5bb877188e45392d484aa450f5e1a6397b3b72ca
Eric Campbell <1234goal@gmail.com>
2020-07-15 14:18:47 -0400

    add slash notation to runtime

--- synthesis/lib/Runtime.ml
+++ synthesis/lib/Runtime.ml
@@ -15,40 +15,53 @@ let matches_of_string (data_str : string) : Match.t list =
   String.split data_str ~on:';'
   |> List.map ~f:(fun match_str ->
       match String.index match_str '[' with
-      | Some _ -> begin match String.lsplit2 match_str ~on:'#' with
-          | Some (vals, sz) ->
+      | Some _ ->
+         begin match String.lsplit2 match_str ~on:'#' with
+         | Some (vals, sz) ->
             let size = int_of_string sz in
             let range = String.strip
-                ~drop:(fun c -> c = '[' || c = ']') vals in
+                          ~drop:(fun c -> c = '[' || c = ']') vals in
             begin match String.lsplit2 range ~on:':' with
-              | Some (hi, lo) -> Match.Between (Int(Bigint.of_string hi, size), Int(Bigint.of_string lo, size))
-              | _ -> Printf.sprintf "Couldn't parse match from string %s" range
-                     |> failwith end
-          | _ -> Printf.sprintf "Couldn't parse match from string %s" match_str
-                 |> failwith end
-      | _ ->
-        begin match String.lsplit2 match_str ~on:'&' with
-          | Some (fst, snd) ->
-            begin match String.lsplit2 snd ~on:'#' with
-              | Some (prefix_str, size_str) ->
-                let size = int_of_string size_str in
-                let mask = Bigint.of_string prefix_str in
-                let addr = Bigint.of_string fst in
-                Match.Mask(Int(addr, size), Int(mask, size))
-              | _ -> Printf.sprintf "Couldn't parse match from string %s" snd
-                     |> failwith
+            | Some (hi, lo) -> Match.Between (Int(Bigint.of_string hi, size), Int(Bigint.of_string lo, size))
+            | _ -> Printf.sprintf "Couldn't parse match from string %s" range
+                   |> failwith
             end
-          | _ ->
-            begin match String.lsplit2 match_str ~on:'#' with
-              | Some (value_str, size_str) ->
-                let size = int_of_string size_str in
-                let value = Bigint.of_string value_str in
-                Match.Exact(Int(value, size))
-              | None ->
-                Printf.sprintf "Couldn't parse match from string %s" match_str
-                |> failwith end
-        end
-    )
+         | _ -> failwith @@ Printf.sprintf "Couldn't parse match from string %s" match_str
+         end
+      | _ ->
+         begin match String.lsplit2 match_str ~on:'&' with
+         | Some (fst, snd) ->
+            begin match String.lsplit2 snd ~on:'#' with
+            | Some (prefix_str, size_str) ->
+               let size = int_of_string size_str in
+               let mask = Bigint.of_string prefix_str in
+               let addr = Bigint.of_string fst in
+               Match.Mask(Int(addr, size), Int(mask, size))
+            | _ -> Printf.sprintf "Couldn't parse match from string %s" match_str
+                   |> failwith
+            end
+         | _ ->
+            begin match String.lsplit2 match_str ~on:'/' with
+            | Some _ ->
+               begin match String.lsplit2 match_str ~on:'#' with
+               | Some (match_str, size) when (size = "32")->
+                  Classbenching.parse_ip_mask match_str
+               | _ -> Printf.sprintf "Couldn't parse match from string %s" match_str
+                      |> failwith
+               end
+            | _ ->
+               begin match String.lsplit2 match_str ~on:'#' with
+               | Some (value_str, size_str) ->
+                  let size = int_of_string size_str in
+                  let value = Bigint.of_string value_str in
+                  Match.Exact(Int(value, size))
+               | None ->
+               Printf.sprintf "Couldn't parse match from string %s" match_str
+               |> failwith
+               end
+            end
+         end
+       )
 
 
 let parse filename : Edit.t list =
