bfc4a7eba9ba9891f52ab00a886b9b8b0e6f8bb2
Bill Hallahan <william.hallahan@yale.edu>
2020-09-13 22:09:36 -0400

    generating multiple files

--- synthesis/whippersnapper/bench.py
+++ synthesis/whippersnapper/bench.py
@@ -5,6 +5,22 @@ import shutil
 import subprocess
 import sys
 
+# fvs gen
+
+def pipeline_fvs(i):
+    return "hdr.ethernet.dstAddr,hdr.ethernet.dstAddr,48\nstandard_metadata.egress_spec,standard_metadata.egress_spec,9"
+
+def set_field_fvs(mx):
+    fvs = "hdr.ptp.reserved2,hdr.ptp.reserved2,8\n";
+    for i in list(range(0, mx)):
+      fvs += "hdr.header_0.field_" + str(i) + ",hdr.header_0.field_" + str(i) + ",16\n"
+    return fvs;
+
+# whippersnapper commands
+def whippersnapper_cmds():
+  return { "set-field" : (["--operations"], [], set_field_fvs)
+         , "pipeline" : (["--tables"], ["--table-size", "16"], pipeline_fvs) }
+
 # generate the OBT, and the corresponding rules
 def rewrite_cmd(cmd):
   try:
@@ -14,30 +30,25 @@ def rewrite_cmd(cmd):
     fparams = ";".join(fparams);
     pieces[2] = fparams;
     #pieces = list(map(lambda s : s[s.find("0x"):] if s.find("0x") != -1 else s, pieces));
-    print("pieces" + str(pieces));
     fcmd = ",".join(pieces);
-    print(fcmd)
     return fcmd;
   except:
     return cmd;
 
 def rewrite_cmds(cmds):
-  print("cmds\n" + cmds + "\n");
   fcmds = list(map(rewrite_cmd, cmds.split("\n")));
   fmcds = "\n".join(fcmds);
-  print("fcmds = " + str(fcmds));
   return fmcds;
 
-def rules_for_obt(fn):
+def rules_for_obt(fn, fvs):
   edits_file = "whippersnapper/empty_edits.txt";
   fvs_file = "output/fvs.txt";
   assume_file = "whippersnapper/empty_assume.txt";
   commands_file = "output/commands.txt";
   commands_no_def_file = "output/commands_no_def.txt"
 
-  with open(fvs_file, 'w') as fvs:
-    fvs.write("hdr.ethernet.dstAddr,hdr.ethernet.dstAddr,48\n");
-    fvs.write("standard_metadata.egress_spec,standard_metadata.egress_spec,9")
+  with open(fvs_file, 'w') as fvsf:
+    fvsf.write(fvs);
   
   with open(commands_file, 'r') as cmds:
     with open(commands_no_def_file, 'w') as cmdnd:
@@ -57,27 +68,28 @@ def rules_for_obt(fn):
   #except:
   #  print("no commands written");
 
-def run_whippersnapper(mx):
-  if not os.path.isdir("whippersnapper/pipelines"):
-    os.mkdir("whippersnapper/pipelines")
+def run_whippersnapper(ws_cmd, mx):
+  if not os.path.isdir("whippersnapper/" + ws_cmd):
+    os.mkdir("whippersnapper/" + ws_cmd)
 
   for i in list(range(1, int(mx))):
     print(str(i));
-    subprocess.run(["p4benchmark", "--feature", "pipeline", "--tables", str(i), "--table-size", "16"]);
+    (cmd_line1, cmd_line2, fvs) = whippersnapper_cmds()[ws_cmd];
+    subprocess.run(["p4benchmark", "--feature", ws_cmd] + cmd_line1 + [str(i)] + cmd_line2);
     subprocess.run(["p4test", "--p4v", "14", "--pp", "output/main16.p4", "output/main.p4"]);
-    rules_for_obt("output");
+    rules_for_obt("output", fvs(i));
     
-    shutil.move("output", "whippersnapper/pipelines/output_" + str(i));
+    shutil.move("output", "whippersnapper/" + ws_cmd + "/output_" + str(i));
 
 # run the actual evaluation, using OBT as the logical program
 
-def run_avenir():
+def run_avenir(ws_cmd):
   mx = 1;
-  while os.path.isdir("whippersnapper/pipelines/output_" + str(mx)):
+  while os.path.isdir("whippersnapper/" + ws_cmd + "/output_" + str(mx)):
     mx += 1;
   
   for i in list(range(1, int(mx))):
-    output = "whippersnapper/pipelines/output_" + str(i) +"/";
+    output = "whippersnapper/" + ws_cmd + "/output_" + str(i) +"/";
     commands_file = output + "obt_commands.txt";
     edits_file = "whippersnapper/empty_edits.txt";
     assume_file = "whippersnapper/empty_assume.txt";
@@ -86,9 +98,10 @@ def run_avenir():
     print(res);
 
 cmd = sys.argv[1];
+ws_cmd = sys.argv[2];
 
 if cmd == "generate":
-  mx = sys.argv[2];
-  run_whippersnapper(mx);
+  mx = sys.argv[3];
+  run_whippersnapper(ws_cmd, mx);
 elif cmd == "eval":
-  run_avenir();
+  run_avenir(ws_cmd);
---
+++ synthesis/whippersnapper/set-field/output_1/main16.p4
@@ -0,0 +1,133 @@
+#include <core.p4>
+#define V1MODEL_VERSION 20200408
+#include <v1model.p4>
+
+header ethernet_t {
+    bit<48> dstAddr;
+    bit<48> srcAddr;
+    bit<16> etherType;
+}
+
+header header_0_t {
+    bit<16> field_0;
+}
+
+header ptp_t {
+    bit<4>  transportSpecific;
+    bit<4>  messageType;
+    bit<4>  reserved;
+    bit<4>  versionPTP;
+    bit<16> messageLength;
+    bit<8>  domainNumber;
+    bit<8>  reserved2;
+    bit<16> flags;
+    bit<64> correction;
+    bit<32> reserved3;
+    bit<80> sourcePortIdentity;
+    bit<16> sequenceId;
+    bit<8>  PTPcontrol;
+    bit<8>  logMessagePeriod;
+    bit<80> originTimestamp;
+}
+
+struct metadata {
+}
+
+struct headers {
+    @name(".ethernet") 
+    ethernet_t ethernet;
+    @name(".header_0") 
+    header_0_t header_0;
+    @name(".ptp") 
+    ptp_t      ptp;
+}
+
+parser ParserImpl(packet_in packet, out headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata) {
+    @name(".parse_ethernet") state parse_ethernet {
+        packet.extract(hdr.ethernet);
+        transition select(hdr.ethernet.etherType) {
+            16w0x88f7: parse_ptp;
+            default: accept;
+        }
+    }
+    @name(".parse_header_0") state parse_header_0 {
+        packet.extract(hdr.header_0);
+        transition select(hdr.header_0.field_0) {
+            default: accept;
+        }
+    }
+    @name(".parse_ptp") state parse_ptp {
+        packet.extract(hdr.ptp);
+        transition select(hdr.ptp.reserved2) {
+            8w1: parse_header_0;
+            default: accept;
+        }
+    }
+    @name(".start") state start {
+        transition parse_ethernet;
+    }
+}
+
+control ingress(inout headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata) {
+    @name(".forward") action forward(bit<9> port) {
+        standard_metadata.egress_spec = port;
+    }
+    @name("._drop") action _drop() {
+        mark_to_drop(standard_metadata);
+    }
+    @name("._nop") action _nop() {
+    }
+    @name(".mod_headers") action mod_headers() {
+        hdr.header_0.field_0 = 16w1;
+    }
+    @name(".forward_table") table forward_table {
+        actions = {
+            forward;
+            _drop;
+        }
+        key = {
+            hdr.ethernet.dstAddr: exact;
+        }
+        size = 4;
+    }
+    @name(".test_tbl") table test_tbl {
+        actions = {
+            _nop;
+            mod_headers;
+        }
+        key = {
+            hdr.ptp.reserved2: exact;
+        }
+        size = 4;
+    }
+    apply {
+        forward_table.apply();
+        test_tbl.apply();
+    }
+}
+
+control egress(inout headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata) {
+    apply {
+    }
+}
+
+control DeparserImpl(packet_out packet, in headers hdr) {
+    apply {
+        packet.emit(hdr.ethernet);
+        packet.emit(hdr.ptp);
+        packet.emit(hdr.header_0);
+    }
+}
+
+control verifyChecksum(inout headers hdr, inout metadata meta) {
+    apply {
+    }
+}
+
+control computeChecksum(inout headers hdr, inout metadata meta) {
+    apply {
+    }
+}
+
+V1Switch(ParserImpl(), verifyChecksum(), ingress(), egress(), computeChecksum(), DeparserImpl()) main;
+
